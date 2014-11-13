a324766c4970058fc8151ba192d5bbcf23c38eaa
bfang <benjamin.kevin.fang@gmail.com>
2014-11-12 18:36:27 -0600

    modify to evaluate combination of primes

--- src/CodeTrial.java
+++ src/CodeTrial.java
@@ -7,24 +7,17 @@ import java.util.*;
  *
  * Original prompt:
  * You are given a Secret which encapsulates a function that accepts a single integer parameter and returns an integer.
- * In Java or a language of your choice, write an application that determines if Secret is an additive function
- * secret(x+y) = secret(x) + secret(y)
- * for all prime numbers less than N where N is a given integer.
+ * In Java or a language of your choice, write an application that determines if Secret.testFunction is an additive function
+ * f(x+y) = f(x) + f(y)
+ * for all combinations of prime numbers less than N where N is a given integer.
  *
  * This implementation uses the console for user input and output. It will accept an integer "N" from the user,
- * and displays a message indicating if secret(N) is additive or not for all prime values at or below N.
- *
- * In this code I assumed that x == y to simplify calculation.
+ * and displays a message indicating if Secret.testFunction(N) is additive or not for all prime values at or below N.
  *
  * I chose Java because it is well documented and is an industry standard making it simple for an individual to modify
  * the function secret for testing.
  */
 public class CodeTrial {
-    //secret function
-    public static int secret(int input){
-        return input;
-    }
-
     public static void main(String args[]){
 
         //User input
@@ -32,21 +25,28 @@ public class CodeTrial {
         System.out.print("Enter an Integer: ");
 
         int num;
-        List<Integer> listOfPrimes;
+        List<Integer> listOfPrimes = new ArrayList<Integer>();
 
         try {
             num = input.nextInt();
             listOfPrimes = runEratosthenesSieve(num);
             boolean isAdditive = true;
+            Secret secret = new Secret();
 
             for (int i = 0; i < listOfPrimes.size(); i++) {
-                int primeValue = listOfPrimes.get(i);
+                for (int k = i + 1; k < listOfPrimes.size() - 1; k++) {
+                    int primeValue = listOfPrimes.get(i);
+                    int primeValue2 = listOfPrimes.get(k);
 
-                //additive check
-                int left = secret(primeValue + primeValue);
-                int right = secret(primeValue) + secret(primeValue);
-                if(left != right){
-                    isAdditive = false;
+                    //additive check
+                    int left = secret.testFunction(primeValue + primeValue2);
+                    int right = secret.testFunction(primeValue) + secret.testFunction(primeValue2);
+                    if (left != right) {
+                        isAdditive = false;
+                        break;
+                    }
+                }
+                if(!isAdditive) {
                     break;
                 }
             }
---
+++ src/Secret.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Benjamin on 11/12/2014.
+ */
+public class Secret {
+    public int testFunction(int input) {
+        return input * input;
+    }
+}
